feea4cfe47888aeba71e02464a4987c965d68d03
alvi3s <yamir2019ymva@gmail.com>
2026-09-08 21:20:02 -0500

    chore: swap local and production API base URLs in environment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sanmartin-backend-d3db.onrender.com/api',
+  apiUrl: 'http://localhost:3000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://sanmartin-backend-d3db.onrender.com/api',
 };
